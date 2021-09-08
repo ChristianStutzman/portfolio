09c0306909caf4d650ae046684a4294ceacf0153
Christian Stutzman <christian.stutzman01@gmail.com>
2021-09-08 15:31:23 -0700

    cleaned code / deleted unnecessary items

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App.jsx';
+// import App from './components/App.jsx';
 import renderParticles from './components/particles.js';
+import Welcome from './components/Welcome.jsx';
+import Portfolio from './components/Portfolio.jsx';
+import Skills from './components/Skills.jsx';
+import About from './components/About.jsx';
 
 renderParticles();
-
-// render(
-// <React.Fragment>
-//   <div id="particles-js"></div>
-//   <App />
-// </React.Fragment>, document.getElementById('app'));
